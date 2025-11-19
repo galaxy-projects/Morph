3b43c80427426f9fc04cb4c9bf288b4f9891764d
reluije <42631255+devreluije@users.noreply.github.com>
2025-11-19 02:15:16 +0100

    mark Comment as experimental

--- core/src/main/java/org/galaxy/morph/annotations/Comment.java
+++ core/src/main/java/org/galaxy/morph/annotations/Comment.java
@@ -1,10 +1,13 @@
 package org.galaxy.morph.annotations;
 
+import org.jetbrains.annotations.ApiStatus;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@ApiStatus.Experimental
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Comment {
